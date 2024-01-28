307a30eacf18a4c4892253e81ba810a5fb237623
rbatuhan <bthnercn99@gmail.com>
2024-01-28 16:32:30 +0300

    Rent a Car

---
+++ RENTACAR/src/App.java
@@ -0,0 +1,13 @@
+import business.UserManager;
+import core.Helper;
+import entity.User;
+import view.AdminView;
+import view.LoginView;
+
+
+public class App {
+    public static void main(String[] args) {
+        Helper.setTheme();
+        LoginView loginView = new LoginView();
+    }
+}
